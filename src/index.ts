efaf24e9dee0805aa165f8081d4720509723a087
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-20 12:25:57 +0530

    Feat made express server with home route only

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("I am live ....!!!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome to the home route." });
+});
+
+app.listen(3000, () => {
+  console.log(`server is running at : http://${"localhost"}:${"3000"}`);
+});
