db161b573fe662978b7df8518ec13846d0979d6a
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-20 16:13:32 +0530

    Added one more thumbnail field in subCategory schema

--- src/models/subCategoryModel.ts
+++ src/models/subCategoryModel.ts
@@ -3,6 +3,7 @@ import { Document, Schema, model } from "mongoose";
 interface IBaseSubCategory {
   title: string;
   categoryId: string;
+  thumbnail: string;
 }
 
 interface ISubCategorySchema extends IBaseSubCategory, Document {}
@@ -10,6 +11,7 @@ interface ISubCategorySchema extends IBaseSubCategory, Document {}
 const subCategorySchema = new Schema<ISubCategorySchema>({
   title: { type: String, required: true, trim: true },
   categoryId: { type: String, required: true, trim: true },
+  thumbnail: { type: String, required: true },
 });
 
 const subCategoryModel = model<ISubCategorySchema>(
