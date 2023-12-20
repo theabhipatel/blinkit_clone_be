1e5d1448a6057f308a3410ac4f3e341667b34bb5
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-20 12:41:27 +0530

    Handled 404 not found routes

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import { HOST_NAME, PORT } from "./config";
 
 const app = express();
 
+/** ---> Handing home route for testing. */
 app.get("/", (req, res) => {
   res
     .status(200)
     .json({ success: true, message: "Welcome to the home route." });
+});
+
+/** ---> Handling Not found (404) routes. */
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: "Route not found." });
 });
 
 app.listen(Number(PORT), HOST_NAME, () => {
